e0592ef29fc2d300a97eba40b34dfb14f2f90c88
Vandan Mishra <shuklapriya418@gmail.com>
2025-09-12 09:22:26 +0530

    1209

--- concepts/asserstions.spec.js
+++ concepts/asserstions.spec.js
@@ -1,5 +1,6 @@
 import {test,expect} from '@playwright/test';
 
+test.describe.configure({mode:'serial'});
 test.describe('learn assertions', ()=>
 {
     test('toBe()' , async({page})=>
@@ -8,5 +9,10 @@ test.describe('learn assertions', ()=>
         await page.goto("https://automationexercise.com/signup");
         const textFromWeb = await page.locator("//h2[contains((normalize-space(text())), 'Login to your account')]").textContent();
         expect(textFromWeb).toBe(textFromExcel);
+    });
+
+    test('toBeTruthy', async({page})=>{
+        const text1=true;
+        expect(text1).toBeTruthy();
     });
 });
